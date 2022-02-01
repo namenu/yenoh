810a87af45c01498b687173325aa7fb763bf8dc2
Hyunwoo Nam <namenu@gmail.com>
2022-02-01 23:30:02 +0900

    Updates

--- js/cljs-runtime/yenoh.ui.js
+++ js/cljs-runtime/yenoh.ui.js
@@ -7,59 +7,59 @@ yenoh.ui.app = (function yenoh$ui$app(){
 var honey__$1 = reagent.core.atom.cljs$core$IFn$_invoke$arity$1("");
 var debug = reagent.core.atom.cljs$core$IFn$_invoke$arity$1("");
 return (function (){
-return new cljs.core.PersistentVector(null, 5, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"form","form",-1624062471),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"textarea","textarea",-650375824),new cljs.core.PersistentArrayMap(null, 4, [new cljs.core.Keyword(null,"rows","rows",850049680),(10),new cljs.core.Keyword(null,"cols","cols",-1914801295),(80),new cljs.core.Keyword(null,"value","value",305978217),cljs.core.deref(yenoh.ui.text),new cljs.core.Keyword(null,"on-change","on-change",-732046149),(function (p1__42975_SHARP_){
-return cljs.core.reset_BANG_(yenoh.ui.text,p1__42975_SHARP_.target.value);
-})], null)], null)], null),new cljs.core.PersistentVector(null, 6, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"input","input",556931961),new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"type","type",1174270348),"button",new cljs.core.Keyword(null,"value","value",305978217),"Convert to HoneySQL",new cljs.core.Keyword(null,"on-click","on-click",1632826543),(function (){
+return new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"class","class",-2030961996),"flex items-center mt-6"], null),new cljs.core.PersistentVector(null, 5, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"class","class",-2030961996),"mx-auto"], null),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"h1","h1",-1896887462),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"class","class",-2030961996),"text-lg font-semibold underline decoration-2 decoration-sky-500"], null),"Online HoneySQL <-> MySQL converter "], null),new cljs.core.PersistentVector(null, 4, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"form","form",-1624062471),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"class","class",-2030961996),"mt-6"], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"textarea","textarea",-650375824),new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"class","class",-2030961996),"form-textarea rounded w-full h-32",new cljs.core.Keyword(null,"value","value",305978217),cljs.core.deref(yenoh.ui.text),new cljs.core.Keyword(null,"on-change","on-change",-732046149),(function (p1__44580_SHARP_){
+return cljs.core.reset_BANG_(yenoh.ui.text,p1__44580_SHARP_.target.value);
+})], null)], null)], null),new cljs.core.PersistentVector(null, 5, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"class","class",-2030961996),"mt-4"], null),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"button","button",1456579943),new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"type","type",1174270348),"button",new cljs.core.Keyword(null,"class","class",-2030961996),"btn-primary",new cljs.core.Keyword(null,"on-click","on-click",1632826543),(function (){
 var s = (function (){var sb__4795__auto__ = (new goog.string.StringBuffer());
-var _STAR_print_newline_STAR__orig_val__42979_42993 = cljs.core._STAR_print_newline_STAR_;
-var _STAR_print_fn_STAR__orig_val__42980_42994 = cljs.core._STAR_print_fn_STAR_;
-var _STAR_print_newline_STAR__temp_val__42981_42995 = true;
-var _STAR_print_fn_STAR__temp_val__42982_42996 = (function (x__4796__auto__){
+var _STAR_print_newline_STAR__orig_val__44582_44592 = cljs.core._STAR_print_newline_STAR_;
+var _STAR_print_fn_STAR__orig_val__44583_44593 = cljs.core._STAR_print_fn_STAR_;
+var _STAR_print_newline_STAR__temp_val__44584_44594 = true;
+var _STAR_print_fn_STAR__temp_val__44585_44595 = (function (x__4796__auto__){
 return sb__4795__auto__.append(x__4796__auto__);
 });
-(cljs.core._STAR_print_newline_STAR_ = _STAR_print_newline_STAR__temp_val__42981_42995);
+(cljs.core._STAR_print_newline_STAR_ = _STAR_print_newline_STAR__temp_val__44584_44594);
 
-(cljs.core._STAR_print_fn_STAR_ = _STAR_print_fn_STAR__temp_val__42982_42996);
+(cljs.core._STAR_print_fn_STAR_ = _STAR_print_fn_STAR__temp_val__44585_44595);
 
-try{cljs.pprint.pprint.cljs$core$IFn$_invoke$arity$1(yenoh.ast__GT_honey((function (){var G__42983 = cljs.core.deref(yenoh.ui.text);
-return (yenoh.parse_select.cljs$core$IFn$_invoke$arity$1 ? yenoh.parse_select.cljs$core$IFn$_invoke$arity$1(G__42983) : yenoh.parse_select.call(null,G__42983));
+try{cljs.pprint.pprint.cljs$core$IFn$_invoke$arity$1(yenoh.ast__GT_honey((function (){var G__44586 = cljs.core.deref(yenoh.ui.text);
+return (yenoh.parse_select.cljs$core$IFn$_invoke$arity$1 ? yenoh.parse_select.cljs$core$IFn$_invoke$arity$1(G__44586) : yenoh.parse_select.call(null,G__44586));
 })()));
-}finally {(cljs.core._STAR_print_fn_STAR_ = _STAR_print_fn_STAR__orig_val__42980_42994);
+}finally {(cljs.core._STAR_print_fn_STAR_ = _STAR_print_fn_STAR__orig_val__44583_44593);
 
-(cljs.core._STAR_print_newline_STAR_ = _STAR_print_newline_STAR__orig_val__42979_42993);
+(cljs.core._STAR_print_newline_STAR_ = _STAR_print_newline_STAR__orig_val__44582_44592);
 }
 return cljs.core.str.cljs$core$IFn$_invoke$arity$1(sb__4795__auto__);
 })();
 return cljs.core.reset_BANG_(honey__$1,s);
-})], null)], null)," ",new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"input","input",556931961),new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"type","type",1174270348),"button",new cljs.core.Keyword(null,"value","value",305978217),"Convert from HoneySQL",new cljs.core.Keyword(null,"on-click","on-click",1632826543),(function (){
+})], null),"Convert to HoneySQL"], null),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"button","button",1456579943),new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"type","type",1174270348),"button",new cljs.core.Keyword(null,"class","class",-2030961996),"btn-primary ml-2",new cljs.core.Keyword(null,"on-click","on-click",1632826543),(function (){
 var hs = cljs.reader.read_string.cljs$core$IFn$_invoke$arity$1(cljs.core.deref(honey__$1));
 var s = yenoh.honey__GT_sql(hs);
 return cljs.core.reset_BANG_(yenoh.ui.text,s);
-})], null)], null)," ",new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"input","input",556931961),new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"type","type",1174270348),"button",new cljs.core.Keyword(null,"value","value",305978217),"debug",new cljs.core.Keyword(null,"on-click","on-click",1632826543),(function (){
+})], null),"Convert from HoneySQL"], null),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"button","button",1456579943),new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"type","type",1174270348),"button",new cljs.core.Keyword(null,"class","class",-2030961996),"btn-primary ml-2",new cljs.core.Keyword(null,"on-click","on-click",1632826543),(function (){
 var s = (function (){var sb__4795__auto__ = (new goog.string.StringBuffer());
-var _STAR_print_newline_STAR__orig_val__42986_42997 = cljs.core._STAR_print_newline_STAR_;
-var _STAR_print_fn_STAR__orig_val__42987_42998 = cljs.core._STAR_print_fn_STAR_;
-var _STAR_print_newline_STAR__temp_val__42988_42999 = true;
-var _STAR_print_fn_STAR__temp_val__42989_43000 = (function (x__4796__auto__){
+var _STAR_print_newline_STAR__orig_val__44587_44596 = cljs.core._STAR_print_newline_STAR_;
+var _STAR_print_fn_STAR__orig_val__44588_44597 = cljs.core._STAR_print_fn_STAR_;
+var _STAR_print_newline_STAR__temp_val__44589_44598 = true;
+var _STAR_print_fn_STAR__temp_val__44590_44599 = (function (x__4796__auto__){
 return sb__4795__auto__.append(x__4796__auto__);
 });
-(cljs.core._STAR_print_newline_STAR_ = _STAR_print_newline_STAR__temp_val__42988_42999);
+(cljs.core._STAR_print_newline_STAR_ = _STAR_print_newline_STAR__temp_val__44589_44598);
 
-(cljs.core._STAR_print_fn_STAR_ = _STAR_print_fn_STAR__temp_val__42989_43000);
+(cljs.core._STAR_print_fn_STAR_ = _STAR_print_fn_STAR__temp_val__44590_44599);
 
-try{cljs.pprint.pprint.cljs$core$IFn$_invoke$arity$1((function (){var G__42992 = cljs.core.deref(yenoh.ui.text);
-return (yenoh.parse_select.cljs$core$IFn$_invoke$arity$1 ? yenoh.parse_select.cljs$core$IFn$_invoke$arity$1(G__42992) : yenoh.parse_select.call(null,G__42992));
+try{cljs.pprint.pprint.cljs$core$IFn$_invoke$arity$1((function (){var G__44591 = cljs.core.deref(yenoh.ui.text);
+return (yenoh.parse_select.cljs$core$IFn$_invoke$arity$1 ? yenoh.parse_select.cljs$core$IFn$_invoke$arity$1(G__44591) : yenoh.parse_select.call(null,G__44591));
 })());
-}finally {(cljs.core._STAR_print_fn_STAR_ = _STAR_print_fn_STAR__orig_val__42987_42998);
+}finally {(cljs.core._STAR_print_fn_STAR_ = _STAR_print_fn_STAR__orig_val__44588_44597);
 
-(cljs.core._STAR_print_newline_STAR_ = _STAR_print_newline_STAR__orig_val__42986_42997);
+(cljs.core._STAR_print_newline_STAR_ = _STAR_print_newline_STAR__orig_val__44587_44596);
 }
 return cljs.core.str.cljs$core$IFn$_invoke$arity$1(sb__4795__auto__);
 })();
 return cljs.core.reset_BANG_(debug,s);
-})], null)], null)], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"textarea","textarea",-650375824),new cljs.core.PersistentArrayMap(null, 4, [new cljs.core.Keyword(null,"rows","rows",850049680),(10),new cljs.core.Keyword(null,"cols","cols",-1914801295),(80),new cljs.core.Keyword(null,"value","value",305978217),cljs.core.deref(honey__$1),new cljs.core.Keyword(null,"on-change","on-change",-732046149),(function (p1__42977_SHARP_){
-return cljs.core.reset_BANG_(honey__$1,p1__42977_SHARP_.target.value);
-})], null)], null)], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"pre","pre",2118456869),cljs.core.deref(debug)], null)], null);
+})], null),"debug"], null)], null),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"div","div",1057191632),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"class","class",-2030961996),"mt-6"], null),new cljs.core.PersistentVector(null, 2, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"textarea","textarea",-650375824),new cljs.core.PersistentArrayMap(null, 3, [new cljs.core.Keyword(null,"class","class",-2030961996),"form-textarea rounded w-full h-32",new cljs.core.Keyword(null,"value","value",305978217),cljs.core.deref(honey__$1),new cljs.core.Keyword(null,"on-change","on-change",-732046149),(function (p1__44581_SHARP_){
+return cljs.core.reset_BANG_(honey__$1,p1__44581_SHARP_.target.value);
+})], null)], null)], null)], null),new cljs.core.PersistentVector(null, 3, 5, cljs.core.PersistentVector.EMPTY_NODE, [new cljs.core.Keyword(null,"pre","pre",2118456869),new cljs.core.PersistentArrayMap(null, 1, [new cljs.core.Keyword(null,"class","class",-2030961996),"mt-4 text-xs border text-sm overflow-auto"], null),cljs.core.deref(debug)], null)], null)], null);
 });
 });
 reagent.dom.render.cljs$core$IFn$_invoke$arity$2(new cljs.core.PersistentVector(null, 1, 5, cljs.core.PersistentVector.EMPTY_NODE, [yenoh.ui.app], null),document.getElementById("app"));
